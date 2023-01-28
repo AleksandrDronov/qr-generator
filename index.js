ad07442af422b2a3a0ad525aa37fecae56d34ae8
Aleksandr Dronov <dronovspb@gmail.com>
2023-01-28 22:32:23 +0600

    add save button

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
-const input = document.querySelector('input');
-const form = document.querySelector('form');
-const image = document.querySelector('img')
+const input = document.querySelector("input");
+const form = document.querySelector("form");
+const image = document.querySelector("img");
+const button = document.querySelector(".save-button");
 
-form.addEventListener('submit', (e) => {
+const getQrcode = async (value) => {
+  const blob = await fetch(
+    `https://api.qrserver.com/v1/create-qr-code/?data=${value}&size=250x250`
+  ).then((res) => res.blob());
+
+  const url = URL.createObjectURL(blob);
+
+  image.src = url;
+  button.href = url;
+};
+
+getQrcode("hello");
+
+form.addEventListener("submit", (e) => {
   e.preventDefault();
-  image.src = `https://api.qrserver.com/v1/create-qr-code/?data=${input.value}&size=250x250`;
-  input.value = ''
-})
+  getQrcode(input.value);
+  input.value = "";
+});
